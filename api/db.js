f44d32b410a073be255b3b352c8fd06f03bde620
Yuchen Cao <caoyc@bu.edu>
2024-06-07 14:57:01 -0400

    Solve a lot of bugs and successfully connect mysql db

--- api/db.js
+++ api/db.js
@@ -1,7 +1,11 @@
-import mysql from "mysql";
-export const db = mysql.createConnection({
-  host: "local",
+import mysql from "mysql2";
+
+export const db = mysql.createPool({
+  host: "localhost",
   user: "root",
   password: "cyc123456",
   database: "blog",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.get("/test", (req, res) => {
-  res.json("It works!");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8800, () => {
   console.log("Connected");
