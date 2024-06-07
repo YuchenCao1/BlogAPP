92b814d37d0c84347b397fc68273186650fabc94
Yuchen Cao <caoyc@bu.edu>
2024-06-07 19:25:21 -0400

    Solve cookies

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //CHECK EXISTING USER
@@ -23,9 +24,36 @@ export const register = (req, res) => {
   });
 };
 
-// export const register = (req, res) => {
-//   const { username, password } = req.body;
-//   res.status(200).json({ message: "User registered successfully" });
-// };
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  //CHECK USER
+
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    //CHECK password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", "1234", {
+        httpOnly: true,
+        path: "/",
+        sameSite: "Lax", // 或 'None' 如果是 HTTPS
+        secure: process.env.NODE_ENV === "production", // 在生产环境中启用 Secure
+      })
+      .status(200)
+      .json(other);
+  });
+};
 export const logout = (req, res) => {};
--- api/index.js
+++ api/index.js
@@ -3,11 +3,18 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
